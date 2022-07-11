161785db9ec07a8d3a2e664dc86591a441203664
endezz <r-serega13@yandex.ru>
2022-07-11 15:40:02 +0400

    refactor: switching icons to svg extension

--- mocks/mocks.ts
+++ mocks/mocks.ts
@@ -2,16 +2,16 @@ export const mocks = [
   {
     id: 1,
     title: 'МТС',
-    logo: '/images/mtc_logo.jpg',
+    logo: '/images/mts-logo.svg',
   },
   {
     id: 2,
     title: 'Мегафон',
-    logo: '/images/megafon_logo.png',
+    logo: '/images/megafon-logo.svg',
   },
   {
     id: 3,
     title: 'Билайн',
-    logo: '/images/beeline_logo.png',
+    logo: '/images/beeline-logo.svg',
   },
 ];
