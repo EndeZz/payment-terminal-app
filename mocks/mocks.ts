2f20666747333106054ffca37fc5641d3cdf3c0a
endezz <r-serega13@yandex.ru>
2022-07-11 15:42:22 +0400

    refactor: changed the location of icons in the folder

--- mocks/mocks.ts
+++ mocks/mocks.ts
@@ -2,16 +2,16 @@ export const mocks = [
   {
     id: 1,
     title: 'МТС',
-    logo: '/images/mts-logo.svg',
+    logo: '/mts-logo.svg',
   },
   {
     id: 2,
     title: 'Мегафон',
-    logo: '/images/megafon-logo.svg',
+    logo: '/megafon-logo.svg',
   },
   {
     id: 3,
     title: 'Билайн',
-    logo: '/images/beeline-logo.svg',
+    logo: '/beeline-logo.svg',
   },
 ];
